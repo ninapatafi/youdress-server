5a95ebdea507a7e60a676387c8c000fd672ee2d1
Nina Patafi <ninapatoofi@gmail.com>
2024-08-16 16:46:49 -0600

    Add default shopping cart item

--- server.js
+++ server.js
@@ -2,11 +2,6 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import fs from "fs";
-// import data from "data/users.json";
-// import * as {type: json} from "./data/products.json";
-
-// const products = productsData;
-// const express = require("express");
 
 const app = express();
 app.use(express.json());
@@ -15,18 +10,7 @@ app.use(express.static("public"));
 const PORT = process.env.PORT || 5050;
 app.use(cors());
 
-// app.use("/", (req, res, next) => {
-//   const filters = req.query;
-//   const filteredProducts = products.filter((product) => {
-//     let isValid = true;
-//     for (key in filters) {
-//       console.log(key, product[key], filters[key]);
-//       isValid = isValid && product[key] == filters[key];
-//     }
-//     return isValid;
-//   });
-//   res.send(filteredUsers);
-// });
+let cart = [];
 
 app.get("/products", (req, res) => {
   const productsBuffer = fs.readFileSync("./data/products.json");
